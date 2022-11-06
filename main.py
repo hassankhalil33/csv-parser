345838b2e0dceee3baa3c1efbb0e34e7e54b35a7
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-06 21:13:16 +0200

    Added Find Average All Function

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import csv
 
-#Init Variables
+#INIT
 
 my_list = []
 
 
-#Functions
+#FUNCTIONS
 
+#find average of all data input
+def find_average_all(array):
+    total = 0
 
+    for i in array:
+        total += i[1]
 
+    return total / len(array)
+        
 
-#Main
+#MAIN
 
 #read csv file
 with open("./test.csv", 'r') as file:
@@ -18,14 +25,12 @@ with open("./test.csv", 'r') as file:
     for row in csvreader:
         r = row[0].split("\t")
         my_list.append(r)
-    
     my_list.pop(0)
-
 
 #remove unneccesary day info and convert string data to float
 for i, l in enumerate(my_list):
     my_list[i][0] = l[0][3:]
     my_list[i][1] = float(l[1])
 
-
 print(my_list)
+print(find_average_all(my_list))
