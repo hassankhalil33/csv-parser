69ead145b9ab7003156b8f710388caeada952de4
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-06 20:57:41 +0200

    Data is Now Saved into List

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import csv
 
+my_dict = []
+
 with open("./test.csv", 'r') as file:
     csvreader = csv.reader(file)
     for row in csvreader:
-        print(row)
+        x = row[0].split("\t")
+        my_dict.append(x)
+    
+    my_dict.pop(0)
+    print(my_dict)
+
+
+
